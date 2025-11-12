355f014b8def62a870e0a4abf166bec326be45a8
Escall <joerenzescallente027@gmail.com>
2025-11-12 23:13:52 +0800

    database restructured

--- database/add_discount_fields.sql
+++
@@ -1,4 +0,0 @@
--- Add discount fields to sales table
-ALTER TABLE `sales` 
-ADD COLUMN `discount_type` VARCHAR(50) DEFAULT NULL AFTER `total_amount`,
-ADD COLUMN `discount_amount` DECIMAL(10,2) DEFAULT 0.00 AFTER `discount_type`;
